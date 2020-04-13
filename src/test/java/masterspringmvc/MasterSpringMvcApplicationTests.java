cd5beae78597a9a9dabe41247adb37873c27cbcc
havywu11 <havywu@wisagetech.com>
2020-04-13 18:22:32 +0800

    first commit

---
+++ src/test/java/masterspringmvc/MasterSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package masterspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
